ab8b25a0949662c0762090972e4f5f70064a4186
RAVEN276 <lintararyadwi@gmail.com>
2026-02-17 21:19:33 +0700

    feat: initialize project with Vite and React setup
    - Added package.json with dependencies for React, Firebase, and development tools.
    - Created public/index.html for Firebase Hosting setup.
    - Added Vite logo SVG and React logo SVG assets.
    - Implemented App component with basic counter functionality and styling.
    - Created CSS styles for the application layout and components.
    - Configured Firebase initialization in src/firebase.ts.
    - Set up main entry point in src/main.jsx for rendering the App component.
    - Added global styles in src/index.css for consistent theming.
    - Configured Vite with a basic setup in vite.config.js.

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyByrVWylTXiW5-Lk1KiStAc70YTteMA5bc",
+  authDomain: "nusa-quest-prototype.firebaseapp.com",
+  projectId: "nusa-quest-prototype",
+  storageBucket: "nusa-quest-prototype.firebasestorage.app",
+  messagingSenderId: "127910489204",
+  appId: "1:127910489204:web:994dc6dfa1de0c5aecb521",
+  measurementId: "G-WY9GMQ457Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
