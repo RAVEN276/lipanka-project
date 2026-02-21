8151dbd9f943bff6dcfed048495a56a2e887493a
Icha Marisa Mahmuda <chaloeymae26@gmail.com>
2026-02-21 15:31:14 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import HeroPage from './HeroPage/HeroPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <HeroPage />
+    </BrowserRouter>
   </StrictMode>,
 )
